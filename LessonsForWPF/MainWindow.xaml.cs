5cdb3f17461b0ffcb3559ad48089ae0e64010904
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-12-02 14:26:33 +0400

    Эвент  кнопок

--- LessonsForWPF/MainWindow.xaml.cs
+++ LessonsForWPF/MainWindow.xaml.cs
@@ -12,7 +12,20 @@ namespace LessonsForWPF
         public MainWindow()
         {
             InitializeComponent();
+
+            btnTest2.Click += BtnTest2_Click; // += tab
+
             // я  мастер  тут  ничего не  будет
+        }
+
+        private void BtnTest2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Эвент  из кода ");
+        }
+
+        private void btnTest_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Эвент  из верстки ");
         }
     }
 }
