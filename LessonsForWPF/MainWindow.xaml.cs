7896ae4741246e764f17661b04a37f374a08c003
РИШАТ Ахтямов <rishat-rus@mail.ru>
2024-12-09 13:43:28 +0400

    Окно  авторизации

--- LessonsForWPF/MainWindow.xaml.cs
+++ LessonsForWPF/MainWindow.xaml.cs
@@ -12,7 +12,28 @@ namespace LessonsForWPF
         public MainWindow()
         {
             InitializeComponent();
-            // я  мастер  тут  ничего не  будет
+            btnAhty.Click += BtnAhty_Click;
+            btnAdd.Click += BtnAdd_Click;
+        }
+
+        private void BtnAhty_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tb_Login.Text;
+            string password = tb_Password.Text;
+
+            if (login == "admin" && password == "123")
+            {
+                MessageBox.Show("Ура");
+            }
+            else
+            {
+                MessageBox.Show("Не  верный логин  и пароль");
+            }
+        }
+
+        private void BtnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("В разработке ");
         }
     }
 }
