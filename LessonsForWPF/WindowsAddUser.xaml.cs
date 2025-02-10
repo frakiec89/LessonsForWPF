2b78edbfb3a18c9de13937039d8ab189c07dc0f6
РИШАТ Ахтямов <rishat-rus@mail.ru>
2025-02-10 15:36:27 +0400

    Валидация

--- LessonsForWPF/WindowsAddUser.xaml.cs
+++ LessonsForWPF/WindowsAddUser.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
+
 
 // track
 
@@ -24,6 +13,29 @@ namespace LessonsForWPF
         public WindowsAddUser()
         {
             InitializeComponent();
+            btnAddUser.Click += BtnAddUser_Click;
+        }
+
+        private void BtnAddUser_Click(object sender, RoutedEventArgs e)
+        {
+           if (string.IsNullOrWhiteSpace(tb_Login.Text) == true)
+           {
+                MessageBox.Show("Введите логин");
+                return;
+           }
+
+            if (tb_Password.Text ==null ||  tb_Password.Text.Length <4)
+            {
+                MessageBox.Show("Пароль меньше 3 символов!");
+                return;
+            }
+
+            if (tb_Password.Text != tb_Password2.Text)
+            {
+                MessageBox.Show("Парали  не совпадают");
+                return;
+            }
+
         }
     }
 }
