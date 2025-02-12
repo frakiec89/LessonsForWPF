edb1f70630d6e207418d9b548a6229b54cd01bbe
РИШАТ Ахтямов <rishat-rus@mail.ru>
2025-02-12 14:50:25 +0400

    up

---
+++ LessonsFor.DB/MyDbContext.cs
@@ -0,0 +1,21 @@
+﻿// track
+
+using Microsoft.EntityFrameworkCore;
+
+namespace LessonsFor.DB
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName=MyDb.db");
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ LessonsFor.DB/User.cs
@@ -0,0 +1,22 @@
+﻿// track
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace LessonsFor.DB
+{
+
+    [Index("Login", IsUnique = true)]
+    public class User
+    {
+
+        [Key]
+        public int  Id { get; set; }
+       
+        public string Login { get; set; }
+
+
+        [Required]
+        [StringLength(100, MinimumLength = 3)]
+        public string Password { get; set; }
+    }
+}
