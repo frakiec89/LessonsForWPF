339c37b2967526fb69cae5cf9268eace4323ffca
РИШАТ Ахтямов <rishat-rus@mail.ru>
2025-02-21 17:21:21 +0400

    up

--- LessonsForWPF/Winds/StudentsWindow.xaml.cs
+++ LessonsForWPF/Winds/StudentsWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿
+using LessonsFor.DB.DBContent;
 using System.Windows;
+using System.Windows.Controls;
 // track
 
 namespace LessonsForWPF.Winds
@@ -15,6 +17,7 @@ namespace LessonsForWPF.Winds
 
             Loaded += StudentsWindow_Loaded;
             Closing += StudentsWindow_Closing;
+            
         }
         
         private void StudentsWindow_Loaded(object sender, RoutedEventArgs e)
@@ -36,6 +39,44 @@ namespace LessonsForWPF.Winds
         {
             Winds.MenuWindows windows = new Winds.MenuWindows();
             windows.Show();
+        }
+
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+
+            if(button != null)
+            {
+                var student = button.DataContext as Student;
+                if(student != null)
+                {
+                    var dialog = MessageBox.
+                        Show($"Вы хотите удалить студента {student.Name} {student.FirstName}\n" +
+                        $"Вы Уверены?"  , "Внимание" , MessageBoxButton.YesNo , MessageBoxImage.Warning);
+
+
+                    if(dialog == MessageBoxResult.Yes)
+                    {
+                        try
+                        {
+                            using LessonsFor.DB.DBContent.MyContentDBContext dBContext = new LessonsFor.DB.DBContent.MyContentDBContext();
+                            dBContext.Students.Remove(student);
+                            dBContext.SaveChanges();
+                            dataGridStudents.ItemsSource = dBContext.Students.ToList();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void btnChange_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
